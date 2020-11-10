0356ccd2010421ee0c5ac0d2cf9e55d637c34399
Vivien Nicolas <vnicolas@apple.com>
2020-11-10 19:47:04 +0100

    Add Color Control cluster tests to Cirque

--- src/test_driver/linux-cirque/ClustersTests.sh
+++ src/test_driver/linux-cirque/ClustersTests.sh
@@ -49,6 +49,7 @@ function main() {
     # Run tests for the all-clusters demo app
     python3 "$SOURCE_DIR/test-on-off-cluster.py"
     python3 "$SOURCE_DIR/test-barrier-control-cluster.py"
+    python3 "$SOURCE_DIR/test-color-control-cluster.py"
 }
 
 source "$SOURCE_DIR"/shell-helpers.sh
