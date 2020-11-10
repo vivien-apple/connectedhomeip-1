79e0c5502b1cca5296d5248b12003ffcab956b3f
Vivien Nicolas <vnicolas@apple.com>
2020-11-10 21:34:55 +0100

    Add Door Lock cluster tests to Cirque

--- src/test_driver/linux-cirque/ClustersTests.sh
+++ src/test_driver/linux-cirque/ClustersTests.sh
@@ -50,6 +50,7 @@ function main() {
     python3 "$SOURCE_DIR/test-on-off-cluster.py"
     python3 "$SOURCE_DIR/test-barrier-control-cluster.py"
     python3 "$SOURCE_DIR/test-color-control-cluster.py"
+    python3 "$SOURCE_DIR/test-door-lock-cluster.py"
 }
 
 source "$SOURCE_DIR"/shell-helpers.sh
