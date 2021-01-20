ddab79579c6bb72b8f22b8726fe28cda54943be6
Vivien Nicolas <vnicolas@apple.com>
2021-01-20 19:39:54 +0100

    Update chip-tool to be a client application using ZAP

--- examples/chip-tool/main.cpp
+++ examples/chip-tool/main.cpp
@@ -29,6 +29,8 @@
 // ================================================================================
 int main(int argc, char * argv[])
 {
+    InitDataModelHandler();
+
     Commands commands;
     registerCommandsPayload(commands);
     registerCommandsPairing(commands);
