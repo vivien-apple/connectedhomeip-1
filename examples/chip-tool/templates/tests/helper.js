9a97fc7d6a16c714f172a07a31223cc1fbf56aaa
Vivien Nicolas <vnicolas@apple.com>
2022-02-21 11:56:28 +0100

    [YAML] Add asRootObject helper that returns the name to access the given property of a decodable type

--- examples/chip-tool/templates/tests/helper.js
+++ examples/chip-tool/templates/tests/helper.js
@@ -15,12 +15,40 @@
  *    limitations under the License.
  */
 
+// Import helpers from zap core
+const zapPath   = '../../../../third_party/zap/repo/dist/src-electron/';
+const appHelper = require('../../../../src/app/zap-templates/templates/app/helper.js');
+
 function utf8StringLength(str)
 {
   return new TextEncoder().encode(str).length
+}
+
+/*
+ * Returns the name to used for accessing a given property of
+ * a decodable type.
+ *
+ */
+function asRootObject(options)
+{
+  // The default name for decodable types is 'value'.
+  let rootObject = 'value';
+
+  // The decodable type for commands is a struct by default, even if the
+  // command just returns a single value.
+  if (this.parent.isCommand) {
+    rootObject += '.' + appHelper.asLowerCamelCase(this.name);
+  }
+
+  if (this.isOptional && !options.hash.forceNotOptional) {
+    rootObject += '.Value()';
+  }
+
+  return rootObject;
 }
 
 //
 // Module exports
 //
 exports.utf8StringLength = utf8StringLength;
+exports.asRootObject     = asRootObject;
